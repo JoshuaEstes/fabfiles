9ebc9516cba80fd4143393fc95d37fa529085f02
Joshua Estes <Joshua.Estes@ScenicCityLabs.com>
2011-09-23 20:25:12 -0500

    added a few descriptions

--- magento.py
+++ magento.py
@@ -2,16 +2,19 @@ from fabric.api import *
 
 @task
 def reindexall():
+  "Reindex Data by all indexers"
   with cd(env.deploy_to):
     run('php shell/indexer.php reindexall')
 
 @task
 def log_status():
+  "Display statsper log tables"
   with cd(env.deploy_to):
     run('php shell/log.php status')
 
 @task
 def log_clean():
+  "Clean the log files"
   with cd(env.deploy_to):
     run('php shell/log.php clean')
 
