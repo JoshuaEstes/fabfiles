55840acf2ed363ac516619e75d319d2589012e65
Joshua Estes <Joshua.Estes@ScenicCityLabs.com>
2012-01-29 20:40:13 -0600

    updated symfony fabfile

--- symfony.py
+++ symfony.py
@@ -1,6 +1,13 @@
 """
+Symfony 1.4 fabfile
+
+1) Make sure you run the task setup for your first deploy. After that you will
+   not need to run setup again.
+
 """
 from fabric.api import *
+from fabric.contrib.project import rsync_project
+from os import getcwd
 
 # Define as many applications as you want here
 env.apps = ['frontend']
@@ -14,13 +21,36 @@ def cache_clear():
     run('./symfony cache:clear')
 
 @task
+def setup():
+  """
+  This needs to be ran first before you start using deploy, once you have ran this
+  once, then you can use deploy as much as you want.
+  """
+  rsync_symfony()
+  setup_databases_yml()
+  doctrine_build_db()
+  doctrine_migrate()
+  plugin_publish_assets()
+
+def setup_databases_yml():
+  """
+  This will setup the databases.yml file
+  """
+  dbhost = prompt('Database Hostname: ',default="127.0.0.1")
+  dbname = prompt('Database Name: ',default='symfony')
+  username = prompt('Database Username: ', default='root')
+  password = prompt('Database Passowrd: ', default='root')
+  with cd(env.deploy_to):
+    run('./symfony configure:database "mysql:host=%s;dbname=%s" "%s" "%s"' % (dbhost, dbname, username, password))
+
+@task
 def deploy():
   """
   Runs project:deploy on your local machine
   Then migrates, optimizes the project, and clears the cache on the remote machines
   """
   project_disable()
-  local("./symfony project:deploy production --go --rsync-options=\"-azvC --force --delete --progress\"")
+  rsync_symfony()
   cache_clear()
 #  doctrine_migrate()
   project_permissions()
@@ -28,10 +58,47 @@ def deploy():
   project_optimize()
   project_enable()
 
+def rsync_symfony():
+  """
+  This will rsync the project
+  """
+  rsync_project(env.deploy_to,getcwd() + '/',[],True,"-pthrvzLaC --exclude-from='" + getcwd() + "/config/rsync_exclude.txt'")
+
+@task
+def doctrine_build(options):
+  """
+  This will run the task doctrine:build with the options that you have given
+  """
+  with cd(env.deploy_to):
+    run('./symfony doctrine:build %s' % options)
+
+def doctrine_build_db():
+  """
+  Build the database
+  """
+  with cd(env.deploy_to):
+    run('./symfony doctrine:build-db')
+
+def doctrine_drop_db():
+  """
+  Drop the database
+  """
+  with cd(env.deploy_to):
+    run('./symfony doctrine:drop-db')
+
 @task
 def doctrine_migrate(version=0):
   """
   Migrate the database
+
+  Usage:
+
+    fab doctrine_migrate
+
+    fab doctrine_migrate:12
+
+  The first example will migrate to the current version. The second example
+  shows how you can migrate to a specific version.
   """
   with cd(env.deploy_to):
     if version > 0 and version.isdigit():
@@ -76,8 +143,8 @@ def project_optimize():
   Optimizes the project
   """
   with cd(env.deploy_to):
-    for app in env.applications:
-      run("./symfony project:optimize prod %s" % app)
+    for app in env.apps:
+      run("./symfony project:optimize %s prod" % app)
 
 @task
 def project_permissions():
